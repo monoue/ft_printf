ade2fe74ee2b68ec628840fde4353a0e8f990b5a
marioyossi <like_a_newborn_baby@icloud.com>
2020-07-28 08:05:53 +0900

    practice added

---
+++ ft_printf_nfukada_0726.c
@@ -0,0 +1,252 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct	s_info
+{
+	int		field;
+	int		prec;
+	char	conv;
+}				t_info;
+
+int	is_digit(char c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t len = ft_strlen(s);
+	char *str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	size_t i = 0;
+	while (str[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
+}
+
+void	init_info(t_info *info)
+{
+	info->field = 0;
+	info->prec = -1;
+	info->conv = '\0';
+}
+
+char *ft_itoa(int n, t_info *info)
+{
+	if (n == 0 && info->prec == 0)
+		return (ft_strdup(""));
+	int len = (n <= 0);
+	int sign = (n >= 0 ? 1 : -1);
+	int tmp = n;
+	while (tmp)
+	{
+		len++;
+		tmp /= 10;
+	}
+	if (info->prec >= len)
+		len = info->prec + (n < 0);
+	char *str = malloc(len + 1);
+	if (str == NULL)
+		return (str);
+	str[len] = '\0';
+	while (len--)
+	{
+		str[len] = n % 10 * sign + '0';
+		n /= 10;
+	}
+	if (sign < 0)
+		str[0] = '-';
+	return (str);
+}
+
+char *ft_utoa(unsigned int n, t_info *info)
+{
+	char *base = "0123456789abcdef";
+	int len = (n == 0);
+	unsigned int tmp = n;
+
+	if (info->prec == 0 && n == 0)
+		return ft_strdup("");
+	while (tmp)
+	{
+		len++;
+		tmp /= 16;
+	}
+	if (info->prec > len)
+		len = info->prec;
+	char *str = malloc(len + 1);
+	if (str == NULL)
+		return (str);
+	str[len] = '\0';
+	while (len--)
+	{
+		str[len] = base[n % 16];
+		n /= 16;
+	}
+	return (str);
+}
+
+int display_s(char *s, t_info *info)
+{
+	int count = 0;
+	if (s == NULL)
+		s = "(null)";
+	int len = ft_strlen(s);
+	if (info->prec != -1 && len > info->prec)
+		len = info->prec;
+	while (info->field > len)
+	{
+		write(1, " ", 1);
+		info->field--;
+		count++;
+	}
+	write(1, s, len);
+	count += len;
+	return (count);
+}
+
+int display_d(int n, t_info *info)
+{
+	int count = 0;
+	char *str = ft_itoa(n, info);
+	if (str == NULL)
+		return (0);
+	int len = ft_strlen(str);
+	while (info->field > len)
+	{
+		write(1, " ", 1);
+		info->field--;
+		count++;
+	}
+	write(1, str, len);
+	free(str);
+	count += len;
+	return (count);
+}
+
+int display_x(unsigned int x, t_info *info)
+{
+	int count = 0;
+	char *str = ft_utoa(x, info);
+	if (str == NULL)
+	{
+		return (0);
+	}
+	int len = ft_strlen(str);
+	while (info->field > len)
+	{
+		write(1, " ", 1);
+		info->field--;
+		count++;
+	}	
+	write(1, str, len);
+	free(str);
+	count += len;
+	return (count);
+}
+
+int display_arg(t_info *info, va_list *ap)
+{
+	if (info->conv == 's')
+		return (display_s(va_arg(*ap, char *), info));
+	if (info->conv == 'd')
+		return (display_d(va_arg(*ap, int), info));
+	if (info->conv == 'x')
+		return (display_x(va_arg(*ap, unsigned int), info));
+	return (0);
+}
+
+int parse_field(const char **str, t_info *info)
+{
+	while (is_digit(**str))
+	{
+		info->field *= 10;
+		info->field += **str - '0';
+		(*str)++;
+	}
+	return (1);
+}
+
+int	parse_prec(const char **str, t_info *info)
+{
+	if (**str != '.')
+	{
+		return (1);
+	}
+	(*str)++;
+	info->prec = 0;
+	while (is_digit(**str))
+	{
+		info->prec *= 10;
+		info->prec += **str - '0';
+		(*str)++;
+	}
+	return (1);
+}
+
+int parse_conv(const char **str, t_info *info)
+{
+	info->conv = **str;
+	if (**str)
+	{
+		(*str)++;
+	}
+	return (1);
+}
+
+int	parse_info(const char **str, va_list *ap)
+{
+	t_info info;
+
+	init_info(&info);
+	(*str)++;
+	parse_field(str, &info);
+	parse_prec(str, &info);
+	parse_conv(str, &info);
+	return (display_arg(&info, ap));
+}
+
+
+int	ft_printf(const char *str, ... )
+{
+	int count = 0;
+	va_list ap;
+
+	va_start(ap, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			int ret = parse_info(&str, &ap);
+			if (ret < 0)
+			{
+				count = -1;
+				break ;
+			}
+			count += ret;
+		}
+		else
+		{
+			write(1, str, 1);
+			count++;
+			str++;
+		}
+	}
+	va_end(ap);
+	return (count);
+}
---
+++ ft_printf_practice.c
@@ -0,0 +1,118 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+#define MIN(x, y) x <= y ? x : y
+#define MAX(x, y) x >= y ? x : y
+#define CTOI(n) n - '0'
+
+typedef struct	s_format_info
+{
+	int		min_width;
+	int		precision;
+	char	conversion_c;
+}				t_format_info;
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+void	init_format_info(t_format_info *format_info)
+{
+	format_info->min_width = 0;
+	format_info->precision = -1;
+	format_info->conversion_c = '`\0';
+}
+
+void	parse_min_width(const char **format, t_format_info *format_info)
+{
+	while (ft_isdigit(**format))
+	{
+		format_info->min_width *= 10;
+		format_info->min_width += CTOI(**format);
+		(*format)++;
+	}
+}
+
+void	parse_precision(const char **format, t_format_info *format_info)
+{
+	if (**format != '.')
+		return ;
+	(*format)++;
+	format_info->precision = 0;
+	while (ft_isdigit(**format))
+	{
+		format_info->precision *= 10;
+		format_info->precision += CTOI(**format);
+		(*format)++;
+	}
+}
+
+void	parse_conversion_c(const char **format, t_format_info *format_info)
+{
+	format_info->conversion_c = **format;
+	if (**format)
+		(*format)++;
+}
+
+int	display_s(char *str, t_format_info *format_info)
+{
+	int	count;
+	int	len;
+
+	count = 0;
+	len	= ft_strlen(str);
+	if (str == NULL)
+		str = "(null)";
+	if (format_info->perc != -1 && len )
+}
+
+int	display_arg(t_format_info *format_info, va_list *args)
+{
+	if (format_info->conversion_c == 's')
+		return (display_s(va_arg(*args, char *), format_info));
+	// if (format_info->conversion_c == 'd')
+	// 	return (display_d(va_arg(*args, int), format_info));
+	// if (format_info->conversion_c == 'x')
+	// 	return (display_x(va_arg(*args, unsigned int), format_info));
+	// return (0);
+}
+
+int	parse_info(const char **format, va_list *args)
+{
+	t_format_info	format_info;
+
+	init_format_info(&format_info);
+	(*format)++;
+	parse_min_width(format, &format_info);
+	parse_precision(format, &format_info);
+	parse_conversion_c(format, &format_info);
+	return (display_arg(&format_info, args));
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		count;
+	va_list	args;
+
+	va_start(args, format);
+	count = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			// format の % のインクリメント等をしたいので、format はポインタで渡している
+			int ret = parse_info(&format, &args);
+		}
+	}
+}
